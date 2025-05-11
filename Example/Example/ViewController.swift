b8ede47f0a8aaf4ffbfc846ff968d77e3559475d
Vlad Suhomlinov <suhomlinov.dev@gmail.com>
2025-05-11 14:56:01 +0300

    Переводит на configure и добавляет mutex

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -15,13 +15,20 @@ class ViewController: UIViewController {
 
         view.backgroundColor = .tertiarySystemBackground
 
-        let meshGradientView1 = MeshGradientView(width: 2, height: 5, colors: [
-            UIColor(red: 255.0/255.0, green: 247.0/255.0, blue: 240.0/255.0, alpha: 1.0), UIColor(red: 255.0/255.0, green: 224.0/255.0, blue: 196.0/255.0, alpha: 1.0),
-            UIColor(red: 254.0/255.0, green: 231.0/255.0, blue: 224.0/255.0, alpha: 1.0), UIColor(red: 255.0/255.0, green: 224.0/255.0, blue: 196.0/255.0, alpha: 1.0),
-            UIColor(red: 249.0/255.0, green: 192.0/255.0, blue: 187.0/255.0, alpha: 1.0), UIColor(red: 241.0/255.0, green: 157.0/255.0, blue: 196.0/255.0, alpha: 1.0),
-            UIColor(red: 246.0/255.0, green: 149.0/255.0, blue: 153.0/255.0, alpha: 1.0), UIColor(red: 249.0/255.0, green: 203.0/255.0, blue: 213.0/255.0, alpha: 1.0),
-            UIColor(red: 249.0/255.0, green: 172.0/255.0, blue: 173.0/255.0, alpha: 1.0), UIColor(red: 254.0/255.0, green: 231.0/255.0, blue: 221.0/255.0, alpha: 1.0)
-        ])!
+        let meshGradientView1 = MeshGradientView()!
+        meshGradientView1.configure(
+            MeshGradientView.MeshGradientViewModel(
+                width: 2,
+                height: 5,
+                colors: [
+                    UIColor(red: 255.0/255.0, green: 247.0/255.0, blue: 240.0/255.0, alpha: 1.0), UIColor(red: 255.0/255.0, green: 224.0/255.0, blue: 196.0/255.0, alpha: 1.0),
+                    UIColor(red: 254.0/255.0, green: 231.0/255.0, blue: 224.0/255.0, alpha: 1.0), UIColor(red: 255.0/255.0, green: 224.0/255.0, blue: 196.0/255.0, alpha: 1.0),
+                    UIColor(red: 249.0/255.0, green: 192.0/255.0, blue: 187.0/255.0, alpha: 1.0), UIColor(red: 241.0/255.0, green: 157.0/255.0, blue: 196.0/255.0, alpha: 1.0),
+                    UIColor(red: 246.0/255.0, green: 149.0/255.0, blue: 153.0/255.0, alpha: 1.0), UIColor(red: 249.0/255.0, green: 203.0/255.0, blue: 213.0/255.0, alpha: 1.0),
+                    UIColor(red: 249.0/255.0, green: 172.0/255.0, blue: 173.0/255.0, alpha: 1.0), UIColor(red: 254.0/255.0, green: 231.0/255.0, blue: 221.0/255.0, alpha: 1.0)
+                ]
+            )
+        )
 
         view.addSubview(meshGradientView1)
 
